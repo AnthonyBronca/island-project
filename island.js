b95787e7244a5246cd238f583e7f6298d2b1d22b
Anthonybronca <bronca1768@gmail.com>
2022-02-24 17:34:10 -0500

    stuff

--- island.js
+++ island.js
@@ -1,48 +1,37 @@
 function getNeighbors(row, col, graph) {
   let neighbors = []
-  let currentPos = [graph[row][col],graph[row][col]]; // <- current position
-  // Check top
-  let top = graph[row-1][col]
-  if(top === 1){
-    neighbors.push([row-1,col])
+  // let top = [row-1][col]
+  // let currentPos = [[row][col]]; // <- current position
+  //if we are in a corner spot (define coners)
+if (row > 0){
+  let top = graph[row - 1][col]
+    if (top === 1) {
+      neighbors.push([row - 1, col])
+    }
   }
-  //top left
-  let topLeft = graph[row-1,col-1]
-  if (topLeft === 1){
-    neighbors.push([row-1,col-1])
+
+  let bottom = graph[row + 1][col]
+  if (bottom) {
+    if (bottom === 1) {
+      neighbors.push([row + 1, col])
+    }
   }
-  //Check topRight
-  let topRight = graph[row-1][col+1]
-  if(topRight === 1){
-    neighbors.push([row-1][col+1])
+
+  let left = graph[row][col - 1]
+  if (left) {
+    if (left === 1) {
+      neighbors.push([row, col - 1])
+    }
   }
-  // Check bottom
-  let bottom = graph[row+1][col]
-  if(bottom === 1){
-    neighbors.push([row+1,col])
-  }
-  // Check left
-  let left = graph[row][col-1]
-  if(left === 1){
-    neighbors.push([row,col-1])
-  }
-//bottom left
-let bottomLeft = graph[row+1][col-1]
-if(bottomLeft === 1){
-  neighbors.push([row+1][col-1])
-}
-//bottom right
-let bottomRight = graph[row+1][col+1]
-if (bottomRight === 1){
-  neighbors.push([row+1][col+1])
-}
   // Check right
-  let right = graph[row][col+1]
-  if(right === 1){
-    neighbors.push([row,col+1])
+  let right = graph[row][col + 1]
+  if (right) {
+    if (right === 1) {
+      neighbors.push([row, col + 1])
+    }
   }
-  // Return neighbors
-return neighbors
+
+  return neighbors
 }
 
 
@@ -58,25 +47,26 @@ function islandSize(row, col, graph) {
 
   // While the stack is not empty,
 
-    // Pop the first node
+  // Pop the first node
 
-    // DO THE THING (increment size by 1)
+  // DO THE THING (increment size by 1)
 
-    // Then push all the UNVISITED neighbors on top of the stack
-    // and mark them as visited
-    // HINT: This is what your helper function `getNeighbors` is for
-    // HINT: Remember, you're storing your visited nodes as strings!
+  // Then push all the UNVISITED neighbors on top of the stack
+  // and mark them as visited
+  // HINT: This is what your helper function `getNeighbors` is for
+  // HINT: Remember, you're storing your visited nodes as strings!
 
   // return size
 
   // Your code here
 }
 let matrix = [
-  [1,1,1,0,0],
-  [0,1,1,0,1],
-  [0,1,1,0,1],
+  [1, 1, 1, 0, 0],
+  [0, 1, 1, 0, 1],
+  [0, 1, 1, 0, 1],
 ]
-console.log(getNeighbors(1, 1, matrix));
+//console.log(getNeighbors(1, 1, matrix));
+console.log(getNeighbors(0,0,matrix))
 
 
 module.exports = [getNeighbors, islandSize];
