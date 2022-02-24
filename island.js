5a2fe28b3fd5f792636988fd782978fb6ff0bafa
Anthonybronca <bronca1768@gmail.com>
2022-02-24 16:44:33 -0500

    started the corners

--- island.js
+++ island.js
@@ -1,16 +1,36 @@
 function getNeighbors(row, col, graph) {
-
+  let neighbors = []
+  let currentPos = [graph[row][col],graph[row][col]]; // <- current position
   // Check top
+  let top = graph[row-1][col]
+  if(top === 1){
+    neighbors.push([row-1,col])
+  }
 
+  //Check topRight
+  let topRight = graph[row-1][col+1]
+  console.log(topRight)
   // Check bottom
-
+  let bottom = graph[row+1][col]
+  if(bottom === 1){
+    neighbors.push([row+1,col])
+  }
   // Check left
+  let left = graph[row][col-1]
+  if(left === 1){
+    neighbors.push([row,col-1])
+  }
+//bottom left
+let bottomLeft = graph[row+1][col-1]
+console.log(bottomLeft)
 
   // Check right
-
+  let right = graph[row][col+1]
+  if(right === 1){
+    neighbors.push([row,col+1])
+  }
   // Return neighbors
-
-  // Your code here
+return neighbors
 }
 
 
@@ -39,5 +59,12 @@ function islandSize(row, col, graph) {
 
   // Your code here
 }
+let matrix = [
+  [1,1,1,0,0],
+  [0,1,1,0,1],
+  [0,1,1,0,1],
+]
+console.log(getNeighbors(1, 1, matrix));
+
 
 module.exports = [getNeighbors, islandSize];
